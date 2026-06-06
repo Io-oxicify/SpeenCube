968a9603a718b12c4890483670877a5339c6519c
Oxic <pipatalt@gmail.com>
2026-06-06 15:01:00 +0700

    added web stuff

--- PAUSE.gd
+++ PAUSE.gd
@@ -3,4 +3,4 @@ extends Button
 func _pressed():
 	var paused = get_tree().paused
 	get_tree().paused = not paused
-	text = "Resume" if not paused else "Pause"
+	text = "Resume" if not paused else "Pause" 
